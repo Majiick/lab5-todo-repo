487cde35efa5fdc7b02d7682d9e897a555f886c9
Zan Smirnov <c15437072@mydit.ie>
2017-10-25 14:43:09 +0000

    l

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
  
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'todotask'
+app.config['MYSQL_DB'] = 'TASKTABLE'
 app.config['MYSQL_HOST'] = '35.187.85.20'
 mysql.init_app(app)
 
